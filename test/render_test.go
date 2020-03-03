40801f66ce4228a97815ca82da24434210a96d0e
Tiexin Guo <tiexin@tiexins-macbook.localdomain>
2020-03-03 18:38:29 +0100

    ut

--- test/render_test.go
+++ test/render_test.go
@@ -21,12 +21,15 @@ func TestRender(t *testing.T) {
 				"vpc_id": "vpc-025c50ddacb9519fd",
 			},
 			rootDir() + "/test/templates/test.tfvars.tpl",
-			rootDir() + "/test/output",
-			rootDir() + "test.tfvars",
+			rootDir() + "/test/output/",
+			"test.tfvars",
 			"",
 		},
 	}
 	for _, test := range tests {
-		tpl.Render(test.context, test.template, test.outputDir, test.outputFilename)
+		err := tpl.Render(test.context, test.template, test.outputDir, test.outputFilename)
+		if err != nil {
+			t.Errorf("Err: %s", err.Error())
+		}
 	}
 }
